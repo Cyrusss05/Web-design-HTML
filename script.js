49657ec00645b1cdc4620bc32cc3d019a1f2bb01
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-07-09 22:54:44 +0800

    js update

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Thank you for your message!');
     this.submit();
+    window.location.href = 'index.html';
 });
